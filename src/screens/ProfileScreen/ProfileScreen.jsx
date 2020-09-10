1ed0a76c63bdd3a35e12b0ee97050ba6747a8dd1
ArcQ <eddielaw296@gmail.com>
2020-09-09 23:23:31 -0400

    refactor: clean up

--- src/screens/ProfileScreen/ProfileScreen.jsx
+++ src/screens/ProfileScreen/ProfileScreen.jsx
@@ -1,11 +1,32 @@
-import { TouchableOpacity, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import PropTypes from 'prop-types';
 import React from 'react';
 
-const style = {};
+import { gStyle } from '../../constants';
+import BaseBottomTabNavigatorLayout from '../../layouts/BaseBottomTabNavigatorLayout';
+import colors from '../../constants/colors';
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    flex: 1,
+    justifyContent: 'center',
+  },
+  text: {
+    ...gStyle.textLarsBold18,
+    color: colors.white,
+  },
+});
 
 export default function ProfileScreen(props) {
-  return <View />;
+  return (
+    <BaseBottomTabNavigatorLayout>
+      <View style={styles.container}>
+        <Text style={styles.text}>Profile</Text>
+      </View>
+    </BaseBottomTabNavigatorLayout>
+  );
 }
 
-ProfileScreen.propTypes = {};
+ProfileScreen.propTypes = {
+};

d3e8fba120204eacf793de2b734c04813355113c
Alanna Tai <alanna.tai@hotmail.com>
2020-10-25 19:18:31 -0400

    Map out base entitles into links list

--- src/components/DrawerContent.js
+++ src/components/DrawerContent.js
@@ -1,4 +1,4 @@
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, TouchableOpacity, Text, Image } from 'react-native';
 import React from 'react';
 import { connect } from 'react-redux';
 import { baseSelectors } from '../store/base/ducks';
@@ -8,31 +8,69 @@ import colors from '../constants/colors';
 
 const styles = StyleSheet.create({
   drawerContentContainer: {
-    alignItems: 'center',
+    backgroundColor: colors.purpleDark,
+    flex: 1,
+  },
+  drawerLinksContainer: {
     backgroundColor: colors.purple,
     flex: 1,
-    justifyContent: 'center',
+    paddingHorizontal: 15,
+  },
+  baseLinkContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 15,
   },
   text: {
     ...gStyle.textLarsBold18,
     color: colors.white,
+    fontSize: 20,
+  },
+  header: {
+    paddingVertical: 20,
+    marginTop: 40,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  headerText: {
+    fontWeight: '600',
+  },
+  image: {
+    height: 60,
+    width: 60,
+    borderRadius: 8,
+    marginRight: 15,
   },
 });
 
-function DrawerContent(props) {
-  return <View style={styles.drawerContentContainer}></View>;
+function DrawerContent({ bases, baseEntities }) {
+  console.log('ENTITIES:', baseEntities);
+  return (
+    <View style={styles.drawerContentContainer}>
+      <View style={styles.header}>
+        <Text style={{ ...styles.text, ...styles.headerText }}>Bases</Text>
+      </View>
+      <View style={styles.drawerLinksContainer}>
+        {bases.map((baseId) => {
+          const base = baseEntities[baseId];
+
+          return (
+            <TouchableOpacity key={baseId} style={styles.baseLinkContainer}>
+              <Image source={{ uri: base.imageUrl }} style={styles.image} />
+              <Text style={styles.text}>{base.name}</Text>
+            </TouchableOpacity>
+          );
+        })}
+      </View>
+    </View>
+  );
 }
 
-const mapStateToProps = (state) => (
-  {
-    bases: baseSelectors.bases(state),
-    baseEntities: baseSelectors.baseEntities(state),
-  }
-);
+const mapStateToProps = (state) => ({
+  bases: baseSelectors.bases(state),
+  baseEntities: baseSelectors.baseEntities(state),
+});
 
 const mapDispatchToProps = {};
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps,
-)(DrawerContent);
+export default connect(mapStateToProps, mapDispatchToProps)(DrawerContent);

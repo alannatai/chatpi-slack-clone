6f369375259b98bd1a9fa32608e642584393c8a4
Alanna Tai <alanna.tai@hotmail.com>
2020-11-11 23:50:59 -0500

    Changed bases drawer to match current slack design - Selected base image shows border style around image

--- src/constants/sharedStyles.js
+++ src/constants/sharedStyles.js
@@ -3,9 +3,18 @@ import fonts from './fonts';
 import gStyle from './gStyle';
 
 export default {
+  text12: {
+    fontSize: 12,
+  },
+  textBold20: {
+    fontSize: 20,
+    fontWeight: '700',
+  },
+  textGray: {
+    color: colors.gray,
+  },
   textWhite20: {
     ...gStyle.textLarsBold20,
-    color: colors.white,
   },
   textWhiteBold20: {
     ...gStyle.textLarsBold20,
